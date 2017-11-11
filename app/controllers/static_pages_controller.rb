06e6273b0b01bf9c10cdf84f0afc2825d831ac14
Connor <connorbailes@gmail.com>
2017-11-11 22:01:29 +0000

    added orders controller and routes

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
   def quiz_view
+  end
   
+  def landing_page
+      @featured_product = Product.first
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
   resources :products
-  get 'quiz/quiz_view'
+
 
   get 'static_pages/about'
-
+  get 'static_pages/landing_page'
   get 'static_pages/index'
   
   get 'quiz/quiz_view'
-  get 'static_pages/index'
-  root 'static_pages#index'
   
+  root 'static_pages#landing_page'
   
+  resources :orders, only: [:index, :show, :create, :destroy]
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
