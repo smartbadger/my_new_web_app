d04ba063e687b9d946e0815a86d2ac418ed71238
Connor <connorbailes@gmail.com>
2017-12-26 21:26:15 +0000

    added preview

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   resources :products
 
-
+  get 'simple_pages/thank_you'
   get 'static_pages/about'
   get 'static_pages/landing_page'
   get 'static_pages/index'
+  get 'static_pages/contact'
+  post 'simple_pages/thank_you'
   
   root 'static_pages#index'
 
