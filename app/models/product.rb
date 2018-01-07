072ce499c0866e32f887b39a2380a254da1ee841
Connor <connorbailes@gmail.com>
2018-01-07 20:19:49 +0000

    fixed 5.2 issue 'ilike'

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,12 @@ class Product < ActiveRecord::Base
     has_many :orders
     has_many :comments
     def self.search(search_term)
-     Product.where("name LIKE ?", "%#{search_term}%")
+     if Rails.env != "production"
+      Product.where("name LIKE ?", "%#{search_term}%")
+     else
+      Product.where("name iLIKE ?", "%#{search_term}%")
+     end
+     
     end
     def highest_rating_comment
      comments.rating_desc.first
