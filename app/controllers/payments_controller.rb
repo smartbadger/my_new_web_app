cbd0bfa821ccf43bbee89a680361f5ffd8954921
Connor <connorbailes@gmail.com>
2018-02-03 05:46:11 +0000

    added stripe

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,31 @@
+
+class PaymentsController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    token = params[:stripeToken]
+    @product = Product.find(params[:product_id])
+    @user = current_user
+
+    # charge card:
+    begin
+      charge = Stripe::Charge.create(
+        :amount => (@product.price * 100).to_i,
+        :currency => "usd",
+        :source => token,
+        :description => params[:stripeEmail],
+        :receipt_email => params[:stripeEmail]
+      )
+      if charge.paid
+        Order.create(product_id: @product.id, user_id: @user.id,
+      total: @product.price.to_i)
+        UserMailer.order_placed(@user, @product).deliver_now
+      end
+
+    rescue Stripe::CardError => e
+      body = e.json_body
+      err = body[:error]
+      flash[:error] = "Unfortunately, there was an error processing your payment: #{err[:message]}"
+    end
+  end
+end
