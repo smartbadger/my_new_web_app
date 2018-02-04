9d466e5145493a5504f148250931b2c07ed89bb2
Connor <connorbailes@gmail.com>
2018-02-04 21:02:07 +0000

    6.9 fix

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,15 +2,11 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
-  	if user.admin?
-      	can :manage, :all
+    user ||= User.new #guest user (not logged in)
+    if user.admin?
+      can :manage, :all 
     else
       can :manage, User, id: user.id
-      cannot :manage, Product
     end
-end
-
-  
+  end
 end
